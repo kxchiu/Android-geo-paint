200adfdc13a0e108c424358110e95ab8c8d2718d
kxchiu <kevinx.chiu@hotmail.com>
2016-02-07 14:23:37 -0800

    add color picker library

--- geopaint/app/src/main/java/edu/uw/cwc8/geopaint/MapsActivity.java
+++ geopaint/app/src/main/java/edu/uw/cwc8/geopaint/MapsActivity.java
@@ -1,12 +1,19 @@
 package edu.uw.cwc8.geopaint;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -22,17 +29,31 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+import org.xdty.preference.colorpicker.ColorPickerDialog;
+import org.xdty.preference.colorpicker.ColorPickerSwatch;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final String TAG = "Map Activity";
 
     private GoogleMap mMap;
+    private Double currentLat;
+    private Double currentLng;
     private UiSettings mUiSettings;
     GoogleApiClient mGoogleApiClient;
     private static final int LOC_REQUEST_CODE = 1;
+
+    private int mSelectedColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        getSupportActionBar();
+
+        mSelectedColor = ContextCompat.getColor(this, R.color.flamingo);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -47,6 +68,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .build();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.draw:
+                Log.v(TAG, "Select draw");
+                showColorPicker();
+                return true;
+            case R.id.share:
+                Log.v(TAG, "Select share");
+                // shareDrawing();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //show color picker
+    // library from: https://github.com/xdtianyu/ColorPicker
+    public void showColorPicker(){
+        int[] mColors = getResources().getIntArray(R.array.default_rainbow);
+
+        ColorPickerDialog dialog = ColorPickerDialog.newInstance(R.string.color_picker_default_title,
+                mColors,
+                mSelectedColor,
+                5, // Number of columns
+                ColorPickerDialog.SIZE_SMALL);
+
+        dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+            @Override
+            public void onColorSelected(int color) {
+                mSelectedColor = color;
+            }
+
+        });
+
+        dialog.show(getFragmentManager(), "color_dialog_test");
     }
 
 
